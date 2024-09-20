ee1b7395b0b183134a56150cce312601ccbfc387
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2024-09-21 00:55:29 +0300

    adaptive home

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,3 @@
-new Swiper('.swiper-benefits', {
-  loop: true,
-  slidesPerView: 1,
-  speed: 1600,
-  navigation: {
-    prevEl: '.prev',
-    nextEl: '.next',
-  },
-  keyboard: {
-    enabled: true,
-  },
-});
+import {bindScrollToLinks} from "./gsap.js";
+
+bindScrollToLinks();
